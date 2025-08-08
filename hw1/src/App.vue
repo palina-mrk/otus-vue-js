6ee0631b44644d1af3f008e3965e8d6227450a5e
palina-mrk <khudziakova@tut.by>
2025-08-08 23:09:11 +0300

    hw1

--- hw1/src/App.vue
+++ hw1/src/App.vue
@@ -1,30 +1,46 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>This is my first project on Vue</h1>
+    <h2>very simple digit counter</h2>
+    <p>Input some word with digits into it:</p>
+    <input type="text" v-model="someWord">
+    <p v-if="someWord.length > 0">Length of the word is: {{ someWord.length }}, sum of digits is {{ sumOfDigits(someWord) }}</p>
+    <p v-else>Input is empty</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup>
+import { ref } from 'vue'
+let someWord = ref('')
+
+let sumOfDigits = (str) => {
+  let res = 0;
+  for(let i = 0; i < str.length; i++)
+    res += (Number(str[i]) ? Number(str[i]) : 0);
+  return res;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+</script>
+
+<style lang="scss" scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 20px 20px 30px;
+  background-color:  wheat;
+  gap: 12px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+h1, h2, p {
+  max-width: 500px;
+  margin: 0;
+  padding: 0;
 }
+
+h1 {
+  font-size: 90%;
+}
+
 </style>
